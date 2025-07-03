427a17109da1c9a465b2aaf888ea3947f3aa9e58
Tyler Beck <tbeck202@gmail.com>
2025-07-03 13:32:34 -0600

    Add Movie and Credits Models

---
+++ FastFilmography/Models/MovieModels/Movie.cs
@@ -0,0 +1,88 @@
+﻿using Newtonsoft.Json;
+
+namespace FastFilmography.Models.MovieModels
+{
+    public class Movie
+    {
+        [JsonProperty("adult")]
+        public bool Adult { get; set; }
+
+        [JsonProperty("backdrop_path")]
+        public string BackdropPath { get; set; }
+
+        [JsonProperty("belongs_to_collection")]
+        public object BelongsToCollection { get; set; }
+
+        [JsonProperty("budget")]
+        public int Budget { get; set; }
+
+        [JsonProperty("genres")]
+        public List<Genre> Genres { get; set; }
+
+        [JsonProperty("homepage")]
+        public string Homepage { get; set; }
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("imdb_id")]
+        public string ImdbId { get; set; }
+
+        [JsonProperty("origin_country")]
+        public List<string> OriginCountry { get; set; }
+
+        [JsonProperty("original_language")]
+        public string OriginalLanguage { get; set; }
+
+        [JsonProperty("original_title")]
+        public string OriginalTitle { get; set; }
+
+        [JsonProperty("overview")]
+        public string Overview { get; set; }
+
+        [JsonProperty("popularity")]
+        public double Popularity { get; set; }
+
+        [JsonProperty("poster_path")]
+        public string PosterPath { get; set; }
+
+        [JsonProperty("production_companies")]
+        public List<ProductionCompany> ProductionCompanies { get; set; }
+
+        [JsonProperty("production_countries")]
+        public List<ProductionCountry> ProductionCountries { get; set; }
+
+        [JsonProperty("release_date")]
+        public string ReleaseDate { get; set; }
+
+        [JsonProperty("revenue")]
+        public int Revenue { get; set; }
+
+        [JsonProperty("runtime")]
+        public int Runtime { get; set; }
+
+        [JsonProperty("spoken_languages")]
+        public List<SpokenLanguage> SpokenLanguages { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        [JsonProperty("tagline")]
+        public string Tagline { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("video")]
+        public bool Video { get; set; }
+
+        [JsonProperty("vote_average")]
+        public double VoteAverage { get; set; }
+
+        [JsonProperty("vote_count")]
+        public int VoteCount { get; set; }
+
+        [JsonProperty("credits")]
+        public Credits Credits { get; set; }
+    }
+}
